c3dd2a8721f91b4e06d4ba18aedd91b815bc8ebe
garvitjain314 <garvitjain314@gmail.com>
2015-07-31 17:03:04 +0530

    Removing sets, sequences and canastas

--- Hand.java
+++ Hand.java
@@ -18,11 +18,14 @@ public class Hand {
 	final static int DIAMOMND = 13;
 	final static int HEART = 26;
 	final static int SPADE = 39;
+	static int naturalSequenceCount = 0;
+	static int sequenceCount = 0;
 	
 	private List<Card> cards = new ArrayList<Card>();
 	private List<Card> jokerCards = new ArrayList<Card>();
+	Deal deal;
 	Hand() {
-		Deal deal = new Deal(numberOfDecks);
+		deal = new Deal(numberOfDecks);
 		Random randomGenerator = new Random();
 	    for (int i = 0; i < cardsInOneHand; ++i){
 	      //int randomCard = randomGenerator.nextInt(numberOfCardsInDeck + numberOfJokersInDeck);
@@ -51,9 +54,9 @@ public class Hand {
 	private int checkRun(int[] availableCards, int size) {
 		int min = size;
 		int indexOfMin = 0;
-		int[] cardsRequiredForRun = new int[14 - size + 1];
+		int[] cardsRequiredForRun = new int[cardsInOneHand + 1 - size + 1];
 		int cardsRequiredForThisRun = size;
-		for(int i = 0; i < 14 - size + 1; i++) {
+		for(int i = 0; i < cardsInOneHand + 1 - size + 1; i++) {
 			cardsRequiredForThisRun = size;
 			for(int j = 0; j < size; j++) {
 				if(availableCards[i + j] > 0) {
@@ -68,32 +71,38 @@ public class Hand {
 			}
 			System.out.println("min -> " + min + " IndexOfMin -> " + indexOfMin);
 		}
-		return min;
-	}
-	
-	private void makeRunForSuit(int suit, int size) {
-		int[] availableCards = new int[14];
-		for(int i = 0; i < cards.size(); i++) {
-			if( cards.get(i).getCardId() / cardsInSuit == suit ) {
-				System.out.println( "cardId ->  " + cards.get(i).getCardId());
-				if( cards.get(i).getCardId() % cardsInSuit == 0 ) {
-					availableCards[cards.size()] += 1;
-				}
-				availableCards[cards.get(i).getCardId() % cardsInSuit] += 1;
-			}
-		}
-		
-		int cardsRequiredForRun = checkRun(availableCards, size);
-		System.out.println("cardsRequiredForRun " + cardsRequiredForRun);
-		
-		for (int i = 0; i < 13; i++) {
-			System.out.println("Suit -> " + suit + " Card -> " + availableCards[i]);
-		}
+		return indexOfMin;
 	}
 	
 	public int makeRun(int size) {
-		for(int i = 0; i < 4; i++) {  //change 1 to 4
-			makeRunForSuit(i, size);
+		for(int p = 0; p < 4; p++) {
+			int suit = p;
+			int[] availableCards = new int[cardsInOneHand + 1];
+			for(int i = 0; i < cards.size(); i++) {
+				if( cards.get(i).getCardId() / cardsInSuit == suit ) {
+					System.out.println( "cardId ->  " + cards.get(i).getCardId());
+					if( cards.get(i).getCardId() % cardsInSuit == 0 ) {
+						availableCards[cardsInOneHand] += 1;
+					}
+					availableCards[cards.get(i).getCardId() % cardsInSuit] += 1;
+				}
+			}
+			
+			int indexOfMin = checkRun(availableCards, size);
+			for(int i = indexOfMin; i < indexOfMin + size; i++) {
+				if(availableCards[i] == 0) {
+					if (deal.Deck[suit * cardsInSuit + i] > 0) {
+						deal.Deck[suit * cardsInSuit + i]--;
+					}
+					else {
+						
+					}
+				}
+			}
+			
+			for (int i = 0; i < 13; i++) {
+				//System.out.println("Suit -> " + suit + " Card -> " + availableCards[i]);
+			}
 		}
 		return 0;
 	}
@@ -103,10 +112,79 @@ public class Hand {
 		return 0;
 	}
 	
+	public void removeSequence(int size) {
+		
+	}
+	
+	public void remove(int cardId) {
+		for(int i = 0; i < cards.size(); i++) {
+			if(cards.get(i).getCardId() == cardId) {
+				cards.remove(i);
+			}
+		}
+	}
+	
+	public boolean exists(int cardId) {
+		for(int i = 0; i < cards.size(); i++) {
+			if(cards.get(i).getCardId() == cardId) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public void removeSets(int size) {
+		int existanceCount = 0;
+		
+		for(int i = 0; i < cardsInSuit; i++) {
+			if(exists(i + cardsInSuit * 0)){
+				existanceCount++;
+			}
+			if(exists(i + cardsInSuit * 1)){
+				existanceCount++;
+			}
+			if(exists(i + cardsInSuit * 2)){
+				existanceCount++;
+			}
+			if(exists(i + cardsInSuit * 3)){
+				existanceCount++;
+			}
+			if(existanceCount == size) {
+				remove(i + cardsInSuit * 0);
+				remove(i + cardsInSuit * 1);
+				remove(i + cardsInSuit * 2);
+				remove(i + cardsInSuit * 3);
+			}
+		}
+	}
+	
+	public void removeCanasta() {
+		boolean canastaFound;
+		do{
+			canastaFound = false;
+			int i;
+			for (i = 0; i <= cards.size() - 3; i++) {
+				if (cards.get(i).isSame(cards.get(i+1)) &&  cards.get(i+1).isSame(cards.get(i+2))) {
+					canastaFound = true; 
+					break;
+				}
+			}
+			if(canastaFound) {
+				naturalSequenceCount++;
+				for(int j = i; j < i + 3; j++) {
+					cards.remove(j);
+				}
+			}
+		}while (canastaFound);
+	}
+	
 	public static void main(String[] args) {
 		Hand hand = new Hand();
 		System.out.println("Helloworld");
 		hand.sortByRank();
+		hand.removeCanasta();
+		hand.removeSequence(3);
+		hand.removeSets(3);
 		int x = hand.makeRun(3);
 	}
 }
