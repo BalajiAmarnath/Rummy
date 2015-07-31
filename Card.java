853e5fa97b7db1855ba1c11770a4f42ff3dc345a
BalajiAmarnath <balajigunyacloud@gmail.com>
2015-07-31 09:49:45 +0530

    Added implementations for constructor, isJoker

--- Card.java
+++ Card.java
@@ -4,10 +4,29 @@ class Card {
 	private char suit;
 	
 	public Card(int id) {
+		cardId = id;
+		id = id % deckSize;
+	
+		if(!isJoker(cardId))
+		{
+			char[] suitSymbols = {'C', 'D', 'H', 'S'};
+			suit = suitSymbols[id / suitSize];
+			rank = id % suitSize;
+		}
 		
 	}
 	
 	public Card getNext() {
-		
+		return null;
+	}
+	
+	public boolean isJoker(int id)
+	{
+		return NoOfDecks*deckSize <= id && id < NoOfDecks*deckSize + NoOfDecks ;
+	}
+	
+	public boolean isJoker()
+	{
+		return NoOfDecks*deckSize <= cardId && cardId < NoOfDecks*deckSize + NoOfDecks ;		
 	}
 }
